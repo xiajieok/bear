db894384b57a02d9ecaa984f8ab634befe049da8
jie.xia <jie.xia@91jyy.com>
2017-06-05 22:22:04 +0800

    enkkkkkkkk

--- bear/settings.py
+++ bear/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'jsc@6&7tn&9k^v2q4n+s+sdt!jyx&=max%uyf2k20)nq97_mj%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -44,7 +44,7 @@ MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+   # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
